074ad87c581b83745a29b108015dc6062e9aae50
Neb <pintaricn@gmail.com>
2023-04-21 10:56:16 +0200

    Old website as default. New website on specific URLs.

--- routes.ts
+++ routes.ts
@@ -1,33 +1,13 @@
 import { CACHE_ASSETS, CACHE_PAGES, CACHE_REDIRECT } from './src/cache'
 import { Router } from '@edgio/core'
 import { nuxtRoutes } from '@edgio/nuxt-nitro'
+import { isProductionBuild } from '@edgio/core/environment'
 
 export default new Router()
 
-  .match('/_nuxt/:path*)', ({renderWithApp, setResponseHeader, cache, proxy}) => {
-    setResponseHeader('sheba', 'testing')
-    cache(CACHE_PAGES)
-    renderWithApp()
-  })
-
-// Legacy
-  .match('/uk/api/:path*', ({ cache, proxy }) => {
-    return proxy('origin', { path: '/uk/api/:path*' })
-  })
-  .match('/uk/header/:path*', ({ cache, proxy }) => {
-    return proxy('origin', { path: '/uk/header/:path*' })
-  })
-  .match('/_ui/:path*', ({ cache, proxy }) => {
-    return proxy('origin', { path: '/_ui/:path*' })
-  })
-  .match('/uk/twccmsservice/:path*', ({ cache, proxy }) => {
-    return proxy('origin', { path: '/uk/twccmsservice/:path*' })
-  })
-  .match('/TWCcommercewebservices/:path*', ({ proxy}) => {
-    return proxy('origin', { path: '/TWCcommercewebservices/:path*' })
-  })
-
+  // **********************
   // Redirects
+  // **********************
   .match(
     {path: '/', cookies: { country: 'us' }},
     ({redirect, cache}) => {
@@ -54,9 +34,12 @@ export default new Router()
     cache(CACHE_REDIRECT)
     redirect('/uk', 301);
   })
+  // **********************
+  // End of Redirects
+  // **********************
 
   // **********************
-  // New Website URLs
+  // Handle New Website
   // **********************
   .match(
     '/uk',
@@ -77,41 +60,37 @@ export default new Router()
     cache(CACHE_REDIRECT)
     renderWithApp()
   })
-
-  .match('/:path(row|manifest|service-worker|assets)', ({renderWithApp, cache}) => {
-    cache(CACHE_PAGES)
-    renderWithApp()
-  })
   // **********************
-  // END of Website URLs
+  // End of Handle New Website
   // **********************
 
-  // Negative matching for proper path in order to avoid redirect
+  // Nuxt Routes for New website
+  .use(nuxtRoutes)
+
+  // **********************
+  // Handle Legacy website
+  // **********************
   .match({path: '/:lang(uk|us)/:path*'}, ({updateResponseHeader, updateUpstreamResponseCookie, proxy}) => {
+      updateResponseHeader('location', /https:\/\/thewhitecompany.com\//gi, '/')
+      updateResponseHeader('location', /https:\/\/www.thewhitecompany.com\//gi, '/')
+      updateUpstreamResponseCookie('geolocation', /domain=.+;/, '')
+      proxy('origin')
+    })
+  .match({ path: '/:path', cookies: { country: 'us'  }},
+    ({ cache, redirect }) => {
+      cache(CACHE_REDIRECT)
+      redirect('/us/' + ':path*', 301);
+    })
+    .match({ path: '/:path' }, ({ cache, redirect }) => {
+      cache(CACHE_REDIRECT)
+      redirect('/uk/' + ':path*', 301);
+    })
+  .match('/:path*', ({setResponseHeader, removeUpstreamResponseHeader, proxy, updateResponseHeader, updateUpstreamResponseCookie }) => {
     updateResponseHeader('location', /https:\/\/thewhitecompany.com\//gi, '/')
     updateResponseHeader('location', /https:\/\/www.thewhitecompany.com\//gi, '/')
     updateUpstreamResponseCookie('geolocation', /domain=.+;/, '')
     proxy('origin')
   })
-  // Redirect for paths that don't have language
-  .match({ path: '/:path((?!.*_nuxt).+)', cookies: { country: 'us'  }},
-  ({ cache, redirect }) => {
-    cache(CACHE_REDIRECT)
-    redirect('/us/' + ':path*', 301);
-  })
-  .match({ path: '/:path((?!.*_nuxt).+)' }, ({ cache, redirect }) => {
-    cache(CACHE_REDIRECT)
-    redirect('/uk/' + ':path*', 301);
-  })
-
-
-.fallback(({ proxy, updateResponseHeader, updateUpstreamResponseCookie }) => {
-  updateResponseHeader('location', /https:\/\/thewhitecompany.com\//gi, '/')
-  updateResponseHeader('location', /https:\/\/www.thewhitecompany.com\//gi, '/')
-  updateUpstreamResponseCookie('geolocation', /domain=.+;/, '')
-  proxy('origin')
-  //serveStatic('404/404.html') // Displays Out of Bounds for all pages that are not part of PIE (needs to be tested and adjusted for each project)
-})
-
-
-// .use(nuxtRoutes)
+  // **********************
+  // End of Handle Legacy website
+  // **********************
